0b24bec9b5d5bf41a8a685ecaf8b313b0f32b395
adriansanchezlarrondo <sanchezlarrondoadrian@fpllefia.com>
2024-01-16 20:00:33 +0100

    Lógica para proyectoNuevoVista.js #14

--- src/vistas/proyectoNuevoVista.js
+++ src/vistas/proyectoNuevoVista.js
@@ -42,5 +42,23 @@ export default {
             </div>
         </div>
     </div>
-    `
+    `,
+  script: () => {
+    console.log('vista registro cargada')
+    // Validación bootstrap
+
+    // Capturamos el formulario en una variable
+    const formulario = document.querySelector('#formRegistro')
+    // Detectamos su evento submit (enviar)
+    formulario.addEventListener('submit', (event) => {
+      // Detenemos el evento enviar (submit)
+      event.preventDefault()
+      event.stopPropagation()
+      // Comprobamos si el formulario no valida
+      if (!formulario.checkValidity()) {
+        // Y añadimos la clase 'was-validate' para que se muestren los mensajes
+        formulario.classList.add('was-validated')
+      }
+    })
+  }
 }

f3aad9caa03557bda5f2ecf1f80884de4275d5fc
adriansanchezlarrondo <sanchezlarrondoadrian@fpllefia.com>
2024-01-16 19:50:46 +0100

    Diseño del aspecto de las vistas #13

--- src/vistas/adminVista.js
+++ src/vistas/adminVista.js
@@ -1,21 +1,91 @@
-export const adminVista = {
+export default {
   template: // html
     `
-    <nav class="navbar bg-secondary fixed-bottom small">
-        <div class="container">
-            <a class="navbar-brand fs-6" href="http://www.fpllefia.com">
-                <img
-                    src="images/logo.svg"
-                    alt="fpllefia"
-                    width="30"
-                    height="24"
-                    class="d-inline-block align-text-top"
-                />
-                FPLlefià
-            </a>
-            <span class="navbar-text">DAW - Asignatura de proyectos</span>
-            <a href="#" class="nav-link">carrebola GITHUB</a>
+    <div class="container mt-5">
+        <h1 class="mb-5 fw-light">Panel de administración</h1>
+        <div class="card rounded-0">
+        <ul class="nav nav-tabs">
+            <li class="nav-item w-50">
+            <a class="nav-link active" href="#">Usuarios</a>
+            </li>
+            <li class="nav-item w-50 bg-secondary">
+            <a class="nav-link" href="panelProyecto.html">Proyectos</a>
+            </li>
+        </ul>
+
+        <div class="container text-center">
+            <div class="row mt-3">
+            <div class="input-group w-50">
+                <i class="fa-solid fa-magnifying-glass p-2 border border-secondary bg-secondary"></i>
+                <input type="text" class="form-control" placeholder="Buscador">
+                <i class="fa-solid fa-x border border-secondary bg-secondary p-2"></i>
+            </div>
+            </div>
+            <table class="table my-5 align-middle text-start">
+            <thead>
+                <tr>
+                <th scope="col"></th>
+                <th scope="col" class="dropdown-toggle">URL imagen</th>
+                <th scope="col" class="dropdown-toggle">Email</th>
+                <th scope="col" class="dropdown-toggle">Nombre</th>
+                <th scope="col" class="dropdown-toggle">Apellidos</th>
+                <th scope="col" class="dropdown-toggle">Fecha</th>
+                <th scope="col" class="dropdown-toggle">Rol</th>
+                <th scope="col" class="dropdown-toggle">Estado</th>
+                <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody class="table-group-divider">
+                <tr>
+                <td scope="row" class="position-relative">
+                    <img src="images/usuario.png" class="img-fluid" width="250" height="250" alt="">
+                    <a href="editar.html"><i class="fa-solid fa-pencil bg-success position-absolute bottom-0 end-0 m-2 rounded-circle p-2 fs-7 text-light"></i></a>
+                </td>
+                <td><input class="" type="text" value="http://imagen.com"></td>
+                <td><input class="" type="text" value="email@email.com"></td>
+                <td><input class="" type="text" value="Nombre usuario"></td>
+                <td><input class="" type="text" value="Apellidos usuario"></td>
+                <td><input class="" type="date"></td>
+                <td><input class="w-75" type="text" value="opción"></td>
+                <td><input class="w-75" type="text" value="opción"></td>
+                <td><a href="editar.html" class="link-underline link-underline-opacity-0"><span class="bg-success text-light p-2">Actualizar</span></a></td>
+                <td><i class="fa-regular fa-trash-can border border-danger p-2 fs-5 text-danger"></i></td>
+                </tr>
+                <tr>
+                <td scope="row" class="position-relative">
+                    <img src="images/usuario.png" class="img-fluid" width="250" height="250" alt="">
+                    <a href="editar.html"><i class="fa-solid fa-pencil bg-success position-absolute bottom-0 end-0 m-2 rounded-circle p-2 fs-7 text-light"></i></a>
+                </td>
+                <td><input class="" type="text" value="http://imagen.com"></td>
+                <td><input class="" type="text" value="email@email.com"></td>
+                <td><input class="" type="text" value="Nombre usuario"></td>
+                <td><input class="" type="text" value="Apellidos usuario"></td>
+                <td><input class="" type="date"></td>
+                <td><input class="w-75" type="text" value="opción"></td>
+                <td><input class="w-75" type="text" value="opción"></td>
+                <td><a href="editar.html" class="link-underline link-underline-opacity-0"><span class="bg-success text-light p-2">Actualizar</span></a></td>
+                <td><i class="fa-regular fa-trash-can border border-danger p-2 fs-5 text-danger"></i></td>
+                </tr>
+                <tr>
+                <td scope="row" class="position-relative">
+                    <img src="images/usuario.png" class="img-fluid" width="250" height="250" alt="">
+                    <a href="editar.html"><i class="fa-solid fa-pencil bg-success position-absolute bottom-0 end-0 m-2 rounded-circle p-2 fs-7 text-light"></i></a>
+                </td>
+                <td><input class="" type="text" value="http://imagen.com"></td>
+                <td><input class="" type="text" value="email@email.com"></td>
+                <td><input class="" type="text" value="Nombre usuario"></td>
+                <td><input class="" type="text" value="Apellidos usuario"></td>
+                <td><input class="" type="date"></td>
+                <td><input class="w-75" type="text" value="opción"></td>
+                <td><input class="w-75" type="text" value="opción"></td>
+                <td><a href="editar.html" class="link-underline link-underline-opacity-0"><span class="bg-success text-light p-2">Actualizar</span></a></td>
+                <td><i class="fa-regular fa-trash-can border border-danger p-2 fs-5 text-danger"></i></td>
+                </tr>
+            </tbody>
+            </table>
         </div>
-    </nav>
+        </div>
+    </div>
+
     `
 }
--- src/vistas/proyectoEditarVista.js
+++ src/vistas/proyectoEditarVista.js
@@ -1,5 +1,53 @@
 export default {
   template: // html
     `
+    <div class="container mt-5">
+        <h1 class="mt-3 mb-4">Edición del proyecto</h1>
+        <div class="text-end">
+        <a href="proyecto.html"><button class="btn border"><i class="fa-solid text-secondary me-2 fa-arrow-right-to-bracket fa-rotate-180"></i>Volver</button></a>
+        </div>
+        <div class="row">
+            <div class="col-md-4 mb-3 mb-md-0">
+                <img class="w-100" src="images/tresraya.jpeg" alt="">
+                <div class="mt-3">
+                    <label for="InputNombre1" class="form-label"><strong>URL imagen:</strong></label>
+                    <input type="text" class="form-control" id="repositorio" value="http://enlaceImagen.com">
+                </div>
+            </div>
+            <div class="col-md-8">
+                <form>
+                    <div class="mb-3">
+                        <label for="nombre" class="form-label"><strong>Nombre:</strong></label>
+                        <input type="text" class="form-control" id="nombre" value="Nombre Autor">
+                    </div>
+                    <div class="mb-3">
+                        <label for="descripcion" class="form-label"><strong>Descripción:</strong></label>
+                        <textarea class="form-control" id="descripcion"
+                            rows="5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam facilis repudiandae, vitae optio a voluptate harum doloribus ipsam laudantium culpa officia beatae quibusdam quaerat nulla rem maxime. Minus, eligendi culpa.</textarea>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 mb-3">
+                            <label for="estado" class="form-label"><strong>Estado:</strong></label>
+                            <input type="text" class="form-control" id="estado" value="Estado">
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="fecha" class="form-label"><strong>Fecha:</strong></label>
+                            <input type="date" class="form-control" id="fecha">
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="enlace" class="form-label"><strong>Enlace:</strong></label>
+                        <input type="text" class="form-control" id="enlace" value="http://enlace.com">
+                    </div>
+                    <div class="mb-3">
+                        <label for="repositorio" class="form-label"><strong>Repositorio:</strong></label>
+                        <input type="text" class="form-control" id="repositorio" value="user.github.com/123456">
+                    </div>
+                    <a href="proyecto.html"><button type="submit" class="btn btn-success">Subir proyecto</button></a>
+                </form>
+            </div>
+        </div>
+    </div>
+
     `
 }

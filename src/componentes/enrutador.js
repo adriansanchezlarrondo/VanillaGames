5fc70f21c68bbaed6903498c34904f049fb5bf31
adriansanchezlarrondo <sanchezlarrondoadrian@fpllefia.com>
2024-01-16 20:20:22 +0100

    objeto rutas actualizado

--- src/componentes/enrutador.js
+++ src/componentes/enrutador.js
@@ -1,5 +1,17 @@
 export const enrutador = {
-  rutas: {},
+  rutas: {
+    home: import('../vistas/homeVista.js'),
+    // Usuarios
+    admin: import('../vistas/adminVista.js'),
+    registro: import('../vistas/registroVista.js'),
+    login: import('../vistas/loginVista.js'),
+    // Proyectos
+    proyectos: import('../vistas/proyectosVista.js'),
+    proyectoNuevo: import('../vistas/proyectoNuevoVista.js'),
+    proyectoEditar: import('../vistas/proyectoEditarVista.js'),
+    proyectoDetalle: import('../vistas/proyectoDetalleVista.js'),
+    404: import('../vistas/404.js')
+  },
   router: () => {},
   observadorRutas: () => {}
 }

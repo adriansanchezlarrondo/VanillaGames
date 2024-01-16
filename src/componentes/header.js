a4e5c9b860a70808278769ae21d922746ba9a7c7
adriansanchezlarrondo <sanchezlarrondoadrian@fpllefia.com>
2024-01-16 20:47:24 +0100

    menu header

--- src/componentes/header.js
+++ src/componentes/header.js
@@ -5,58 +5,52 @@ export const header = {
   template: // html
   ` 
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div class="container">
-          <a class="navbar-brand" href="#/home"
-              ><img
-                  src="images/logo.svg"
-                  alt=""
-                  width="30"
-                  height="24"
-                  class="d-inline-block align-text-top router-link"
-              />
+    <div class="container">
+        <a class="navbar-brand" href="#/home"
+        ><img
+            src="images/logo.svg"
+            alt=""
+            width="30"
+            height="24"
+            class="d-inline-block align-text-top"
+        />
+        Vanilla Games</a
+        >
+        <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        >
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        
+        <!-- Menu común para todos los usuarios -->
+        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#/home">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">TOP5 Proyectos</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">A cerca de</a>
+            </li>
+        </ul>
 
-              Vanilla Games</a
-          >
-          <button
-              class="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-          >
-              <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                      <a class="nav-link active router-link" aria-current="page" href="#/home">Home</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link" aria-current="page" href="#">TOP5 Proyectos</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link" aria-current="page" href="#">A cerca de</a>
-                  </li>
-              </ul>
-              <ul class="navbar-nav ms-auto me-2 mb-2 mb-lg-0">
-                  <li class="nav-item">
-                      <a class="ms-2 btn btn-success router-link" href="#/login">
-                          Iniciar sesión
-                          <i class="bi bi-box-arrow-in-right"></i>
-                      </a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="ms-2 btn btn-outline-light router-link" href="#/registro">
-                          Regístrate
-                          <i class="bi bi-box-arrow-in-right"></i>
-                      </a>
-                  </li>
-              </ul>
-          </div>
-      </div>
-  </nav>
+        <!-- Aquí va el Menu rol -->
+        <div id="menuRol"></div>
+        
+        <!-- Aquí va el Menu usuario -->
+        <div id="menuUsuario"></div>
+        
+        </div>
+    </div>
+    </nav>
   `,
   script: () => {
     console.log('Header cargado')

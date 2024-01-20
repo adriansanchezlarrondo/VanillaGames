b987989778b2e273343a587b0bdb41f03f14c37d
adriansanchezlarrondo <sanchezlarrondoadrian@gmail.com>
2024-01-20 20:58:47 +0100

    resto de menus admin y desarrollador #16

--- src/componentes/menus.js
+++ src/componentes/menus.js
@@ -29,9 +29,21 @@ const menuRol = {
     `,
   templateDesarrollador: // html
     `
+    <ul class="navbar-nav ms-auto me-2 mb-2 mb-lg-0">
+        <li class="nav-item">
+        <a class="nav-link active router-link" aria-current="page" href="#/proyectos">PROYECTOS</a>
+        </li>
+        
+    </ul>
     `,
   templateAdmin: // html
     `
+    <ul class="navbar-nav ms-auto me-2 mb-2 mb-lg-0">
+        <li class="nav-item">
+        <a class="nav-link active router-link" aria-current="page" href="#/proyectos">PROYECTOS</a>
+        </li>
+        <!-- panel admin -->
+    </ul>
     `
 }
 
@@ -66,9 +78,59 @@ const menuUsuario = {
     `,
   templateDesarrollador: // html
     `
+    <ul class="navbar-nav ms-auto me-2 mb-2 mb-lg-0">
+        <li class="nav-item dropdown">
+        <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+        >
+            <img src="images/usuario.png" alt="" width="25" />
+        </a>
+        <ul class="dropdown-menu me-0" style="left: -100px; width: 100px">
+            <li class="text-light text-end p-2 small">
+            ${ls.getUsuario().email}
+            </li>
+            <li class="text-light text-end pe-2 small fst-italic">
+            ${ls.getUsuario().rol}
+            </li>
+            <li><hr class="dropdown-divider" /></li>
+            <li><a class="dropdown-item" href="#">Mi perfil</a></li>
+            <li><hr class="dropdown-divider" /></li>
+            <li><a class="dropdown-item" href="#">Cerrar sesión</a></li>
+        </ul>
+        </li>
+    </ul>
     `,
   templateAdmin: // html
     `
+    <ul class="navbar-nav ms-auto me-2 mb-2 mb-lg-0">
+        <li class="nav-item dropdown">
+        <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+        >
+            <img src="images/usuario.png" alt="" width="25" />
+        </a>
+        <ul class="dropdown-menu me-0" style="left: -100px; width: 100px">
+            <li class="text-light text-end p-2 small">
+            ${ls.getUsuario().email}
+            </li>
+            <li class="text-light text-end pe-2 small fst-italic">
+            ${ls.getUsuario().rol}
+            </li>
+            <li><hr class="dropdown-divider" /></li>
+            <li><a class="dropdown-item" href="#">Mi perfil</a></li>
+            <li><hr class="dropdown-divider" /></li>
+            <li><a class="dropdown-item" href="#">Cerrar sesión</a></li>
+        </ul>
+        </li>
+    </ul>
     `
 }
 
